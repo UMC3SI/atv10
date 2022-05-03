5aa8ddd4fea164f4923d5fe54ba4be45b6b23f0a
GABRIEL FERRI <gabriel.ferri@outlook.com>
2022-05-02 21:50:58 -0300

    Atividade 10
    11211102207

---
+++ atividade10/src/atividade10/Espeon.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade10;
+
+/**
+ *
+  * @author @Gabriel ferri(11211102207)- @Gabriel Monteiro(11211102064)
+ */
+public class Espeon extends evee {
+    
+    @Override
+    public String Ataque() {
+        return ("Psychic -   Psybeam");
+    }
+
+    @Override
+    public String Especial() {
+        return (" Psychic -  Psychic");
+    }
+    @Override
+    
+    public String Defesa() {
+        return ("Psychic -  Confusion");
+    }
+    
+}
---
+++ atividade10/src/atividade10/Leafeon.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade10;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class Leafeon extends evee {
+    
+    @Override
+    public String Ataque() {
+        return ("normal -  Quick Attack");
+    }
+
+    @Override
+    public String Especial() {
+        return ("Grass -  Solar Beam");
+    }
+
+    @Override
+    public String Defesa() {
+        return ("grass -  Bullet Seed ");
+    }
+    
+}
