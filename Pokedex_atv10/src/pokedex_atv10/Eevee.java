a035da4c8b40d23c505310b3a65baddb30e5ccff
Pho3nix999 <100255377+Pho3nix999@users.noreply.github.com>
2022-05-02 21:43:44 -0300

    Init commit
    atv 10 pokedex
    
    Co-Authored-By: renatacristina <80224209+RenataCristina@users.noreply.github.com>

---
+++ Pokedex_atv10/src/pokedex_atv10/Eevee.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokedex_atv10;
+
+/**
+ * Autor Leonardo Piascentini Bugmann RGM 11211100076
+ * Co-Autor Renata Cristina da Silva RGM 11211100766
+ */
+public class Eevee {
+    String tipo;
+    double peso;
+    int altura;
+    int hp;
+
+    public String ataque() {
+        return "Quick Attack";
+    }
+
+    public String especial() {
+        return "Weather  Ball";
+    }
+
+    public String defesa() {
+        return "Agility";
+    }
+}
---
+++ Pokedex_atv10/src/pokedex_atv10/Espeon.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokedex_atv10;
+
+/**
+ * Autor Leonardo Piascentini Bugmann RGM 11211100076
+ * Co-Autor Renata Cristina da Silva RGM 11211100766
+ */
+public class Espeon extends Eevee {
+
+    @Override
+    public String ataque() {
+        return "Zen Headbutt";
+    }
+
+    @Override
+    public String especial() {
+        return "Psyshock";
+    }
+
+    @Override
+    public String defesa() {
+        return "Ally Switch";
+    }
+}
